155059d2b7a7f551752ea004b475ee605fb091af
Warren Harper <warrenjharper@gmail.com>
2013-09-23 09:47:04 -0400

    Change models to match start.py

--- map_site/heat_map/models.py
+++ map_site/heat_map/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
+    name = models.TextField()
+    location = models.TextField()
+    numcommits = model.IntegerField()
+
     def __unicode__(self):
         return self.name
         
